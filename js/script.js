d5eb30eae158d509bfabcf3855c6ee1f01493d1e
Young <abiodunadebambo44@gmail.com>
2020-11-17 01:37:41 +0100

    finished with the upload and result page

---
+++ js/script.js
@@ -0,0 +1,121 @@
+// get the file input element and the field
+const fileInput = document.getElementById('data');
+const fileInputField = fileInput.closest('.file-input-field');
+const cardList = document.querySelector('.upload-cards'); 
+
+// add click event listener to the card list and delegate to the buttons
+cardList.addEventListener('click', e => {
+  // if the target is a cancel button
+  if (e.target.closest('.cancel-button')) {
+    e.preventDefault()
+    // remove the card from the DOM
+    e.target.closest('.upload-card').remove();
+    // remove from the upload server too
+    
+    // if there are no more cards, remove the check btn and add information text
+    if (!cardList.querySelector('.upload-card')) {
+      document.querySelector('.check-btn').classList.add('d-none');
+      document.querySelector('.info-text').classList.remove('d-none');
+    }
+  }
+})
+
+fileInput.addEventListener('change', e => {
+  handleFilesInput(e.target.files)
+})
+// when anything is dragged over the surface of the field add the modifier class
+fileInputField.addEventListener('dragover', e => {
+  e.preventDefault()
+  fileInputField.classList.add('file-input-field--over');
+});
+
+// when the drag is ended or left, remove the modifier class
+['dragend', 'dragleave'].forEach(evt => {
+  fileInputField.addEventListener(evt, e => {
+    e.preventDefault();
+    fileInputField.classList.remove('file-input-field--over');
+  })
+})
+
+// if the item is dropped on the field
+fileInputField.addEventListener('drop', e => {
+  e.preventDefault();
+
+  if(e.dataTransfer.files.length) {
+    // insert the file in the input element
+    fileInput.files = e.dataTransfer.files;
+    // handle the drop on the field to display cards
+    handleFilesInput(fileInput.files);
+  }
+  // remove the modifier class
+  fileInputField.classList.remove('file-input-field--over');
+})
+
+// handle the files dropping
+const handleFilesInput = files => {
+  // clear the cardlist of an cards / backend - stop the upload of all files 
+  if (document.querySelector('.upload-card')) {
+    document.querySelectorAll('.upload-card').forEach(card => {
+      card.remove();
+    });
+  }
+
+  // show btn
+  document.querySelector('.check-btn').classList.remove('d-none');
+  // remove information text
+  document.querySelector('.info-text').classList.add('d-none');
+
+  // add preloader
+  showPreloader();
+
+  // run through all the properties of the filelist and select the actual files
+  for (file in files) {
+    if (files[file].type){
+      // handle the rendering of each file
+      handleFilesRender(files[file]);
+    }
+  }
+}
+// handle rendering of the files
+const handleFilesRender = file => {
+  // only create card if it's an image alone
+  if (file.type.startsWith('image/')) {
+    // instantiate a file reader to get the file URL
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file)
+    fileReader.onload = () => {
+      // when the file reader reads the file, create the card
+      card = `
+      <div class="mb-3 rounded media container upload-card bg-white" data-card-id="${file.name}">
+        <div class="row">
+          <div class="align-self-center img-container col-3">
+            <img src="${fileReader.result}" class=" img-thumbnail border-0" alt="">
+          </div>
+          <div class="col-10 media-body p-2 upload-card-body">
+            <button class="border-0 bg-transparent cancel-button"><span class="fas fa-times text-danger"></span></button>
+            <p class="darkblue-text font-weight-bold card-title text-truncate">${file.name}</p>
+            <div class="progress w-75" style="height: 8px;">
+              <div class="progress-bar bg-primary rounded-pill" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p class="darkblue-text font-weight-bold upload-status mt-3 mb-0">Upload status</p>
+          </div>
+        </div>
+      </div>
+      `
+      // after card creation render the file
+      renderCard(card)
+    }
+  }
+}
+// render the card and remoive the preloader
+const renderCard = card => {
+  hidePreloader();
+  cardList.innerHTML += card;
+}
+const showPreloader = () => {
+  document.querySelector('.preloader').classList.remove('d-none');
+}
+const hidePreloader = () => {
+  document.querySelector('.preloader').classList.add('d-none');
+
+}
